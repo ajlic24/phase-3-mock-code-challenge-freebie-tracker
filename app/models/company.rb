7bd743c2cc727cfb0b197554644a4eb0e6fcd004
ajlic24@gmail.com <ajlic24@gmail.com>
2022-03-21 12:04:31 -0400

    commit tree f0a08ffc688542a18f829985b30edce97d12741d

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,13 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie dev, item_name, value
+        freebies << Freebie.create(dev: dev, item_name: item_name, value: value)
+    end
+
+    def self.oldest_company
+        earliest = self.minimum("founding_year")
+        self.find_by(founding_year: earliest)
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies 
+    has_many :companies, through: :freebies 
 
+    def received_one? item_name
+        freebies_arr = ""
+        self.freebies.each do |f|
+            freebies_arr << f.item_name
+        end
+        if freebies_arr.include? item_name
+            true
+        else false
+        end
+    end
+
+    def give_away dev, freebie
+        if self.received_one? freebie.item_name
+            dev.freebies << freebie
+        else
+            nil
+        end
+    end
 end
